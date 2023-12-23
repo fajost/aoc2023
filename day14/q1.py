301d5d1f2dc89bb2c9d2285d21181f06bc50200a
Fabian Jost <mail@fjost.de>
2023-12-23 16:12:32 +0100

    Solved day 14, question 1

--- day14/q1.py
+++ day14/q1.py
@@ -1,12 +1,47 @@
 from pathlib import Path
 
-SOURCE = Path("test.txt")
+SOURCE = Path("input.txt")
+
+
+def move_upwards(grid, col, row, start, count):
+    for update_row in range(start, row):
+        if update_row - start < count:
+            grid[update_row][col] = "O"
+        else:
+            grid[update_row][col] = "."
+
+
+def roll_north(grid):
+    for col in range(len(grid[0])):
+        start = 0
+        count = 0
+        for row in range(len(grid)):
+            if grid[row][col] == "O":
+                count += 1
+            elif grid[row][col] == "#":
+                move_upwards(grid, col, row, start, count)
+                start = row + 1
+                count = 0
+        move_upwards(grid, col, len(grid), start, count)
+    return grid
+
+
+def calc_load(grid):
+    load = 0
+    total_rows = len(grid)
+    for idx, row in enumerate(grid):
+        load += sum([val == "O" for val in row]) * (total_rows - idx)
+    return load
 
 
 def main():
     with SOURCE.open("r") as file:
         lines = file.read().splitlines()
-        print(lines)
+    grid = [[val for val in row] for row in lines]
+    grid = roll_north(grid)
+    for row in grid:
+        print("".join(row))
+    print(calc_load(grid))
 
 
 if __name__ == "__main__":
